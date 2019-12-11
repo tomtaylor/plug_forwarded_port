cd74c84540b17f57ada0e1935053da77855e4961
Tom Taylor <tom@tomtaylor.co.uk>
2019-12-11 10:11:22 +0000

    Prepare for Hex release

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 Plug for parsing the `X-Forwarded-Port` header and setting the `Plug.Conn` port from it.
 
+I use this on Heroku to set the port correctly, so `Plug.Conn.request_url/1` generates the URL correctly.
+
 ## Installation
 
 ```elixir
 def deps do
   [
-    {:plug_forwarded_port, "~> 0.1.0"}
+    {:plug_forwarded_port, "~> 0.1.0"},
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,27 +1,38 @@
 defmodule PlugForwardedPort.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :plug_forwarded_port,
-      version: "0.1.0",
-      elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
+      version: @version,
+      elixir: "~> 1.8",
+      name: "PlugForwardedPort",
+      description: "Sets the Plug.Conn port from the X-Forwarded-Port HTTP header",
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Tom Taylor"],
+      links: %{"GitHub" => "https://github.com/tomtaylor/plug_forwarded_port"}
+    ]
+  end
+
   defp deps do
     [
-      {:plug, "~> 1.8"}
+      {:plug, "~> 1.8"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
